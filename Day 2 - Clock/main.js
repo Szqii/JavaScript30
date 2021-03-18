244a9af0d3a9f7413ea5dd9cbd6ba04123be06ad
Szqii <szqii.altn@gmail.com>
2021-03-18 22:07:08 +0300

    Update App 🔨

--- Day 2 - Clock/main.js
+++ Day 2 - Clock/main.js
@@ -2,12 +2,27 @@ const secondHand = document.querySelector(".sec-hand");
 const minHand = document.querySelector(".min-hand");
 const hourHand = document.querySelector(".hour-hand");
 
+const audio = document.getElementById("tick");
+
+function mute(){
+    audio.muted = true;
+}
+function unmute(){
+    audio.muted = false;
+}
+mute();
+
+
 function setDate() {
   const now = new Date();
 
   const second = now.getSeconds();
   const secondDegrees = second * 6 + 90;
   secondHand.style.transform = `rotate(${secondDegrees}deg)`;
+  if (!audio.hasAttribute("muted")) {
+    audio.currentTime = 0;
+    audio.play();
+  }
 
   const minute = now.getMinutes();
   const minDegrees = minute * 6 + second / 10 + 90;
@@ -24,15 +39,30 @@ setDate();
 
 // Theme Change
 
-document.getElementById('green').onclick = function () { 
-    document.getElementById('theme-css').href = './style/default.css';
+document.getElementById("green").onclick = function () {
+  document.getElementById("theme-css").href = "./style/default.css";
 };
-document.getElementById('red').onclick = function () { 
-    document.getElementById('theme-css').href = './style/red.css';
+document.getElementById("red").onclick = function () {
+  document.getElementById("theme-css").href = "./style/red.css";
 };
-document.getElementById('blue').onclick = function () { 
-    document.getElementById('theme-css').href = './style/blue.css';
+document.getElementById("blue").onclick = function () {
+  document.getElementById("theme-css").href = "./style/blue.css";
 };
-document.getElementById('bw').onclick = function () { 
-    document.getElementById('theme-css').href = './style/bw.css';
+document.getElementById("bw").onclick = function () {
+  document.getElementById("theme-css").href = "./style/bw.css";
+};
+
+// Mute Status
+const muteIcon = document.getElementById("mute");
+
+muteIcon.onclick = function () {
+  if (muteIcon.classList.contains("fa-volume-mute")) {
+    muteIcon.classList.remove("fa-volume-mute");
+    muteIcon.classList.add("fa-volume-up");
+    unmute();
+  } else {
+    muteIcon.classList.add("fa-volume-mute");
+    muteIcon.classList.remove("fa-volume-up");
+    mute();
+  }
 };
